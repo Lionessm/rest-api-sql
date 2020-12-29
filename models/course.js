18ae0575af4a513b385dc9e69066d13f93df2efd
Maria <maria.mihaila.17@gmail.com>
2020-12-29 18:17:36 +0200

    Create the course model

---
+++ models/course.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+
+    class Course extends Model {}
+    Course.init({
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A name is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a name'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A description is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a description'
+                }
+            }
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                msg: 'The email you entered already exists'
+            },
+            validate: {
+                notNull: {
+                    msg: 'An email is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide an email'
+                }
+            }
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        userId: {
+
+        },
+
+    }, { sequelize });
+
+    return Course;
+    };
